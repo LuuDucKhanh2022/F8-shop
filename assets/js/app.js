b2139228f8ec222786bdb6fb9a857352dd8e8c79
LuuDucKhanh2022 <luuduckhanharsenal@.com>
2022-05-21 20:10:36 +0700

    version 1.0

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+let leftSidebar = document.querySelector('.left-sidebar')
+let leftSidebarBtn = document.querySelector('.left-sidebar-btn')
+
+
+function toggleLeftSidebar() {
+    leftSidebar.classList.toggle("open")
+    leftSidebarBtn.classList.toggle("open")
+
+}
